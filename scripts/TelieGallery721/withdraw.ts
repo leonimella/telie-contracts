090a75135fddd3a73356b2c940844a0c3ebe0546
Leoni Mella <leoni.mella@gmail.com>
2023-02-12 13:30:59 -0300

    chore: add comment in withdraw script

--- scripts/TelieGallery721/withdraw.ts
+++ scripts/TelieGallery721/withdraw.ts
@@ -3,9 +3,14 @@ import telieJSON from "../../artifacts/contracts/TelieGallery721/TelieGallery721
 
 async function main() {
   const address = "0x1281C9b875CE53393D2b9563884a34A785c44168"; // MUMBAI
-  // const address = "TBD"; // POLYGON
+  // const address = "0x4fd8592c0b1dc23ed7a05604082b0b0061c36173"; // POLYGON
   const [owner] = await ethers.getSigners();
   const telie721 = new ethers.Contract(address, telieJSON.abi, owner);
+
+  // MUMBAI (Wrong function name, need to update contract first)
+  // const tx = await telie721.withdrawal();
+
+  // MATIC
   const tx = await telie721.withdraw();
 
   console.log("Awaiting transaction confirmation:", tx.hash);
